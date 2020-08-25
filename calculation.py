99c5d73026112040856d33986226a628f74962ef
starfishkenny <starfish8913@naver.com>
2020-08-25 10:24:52 +0900

    defined calculation

---
+++ calculation.py
@@ -0,0 +1,17 @@
+# -*-coding:utf-8-*-
+
+def plus(a,b):
+    c = a+b
+    return c
+
+def subtract(a,b):
+    c = a-b
+    return c
+
+def multiple(a,b):
+    c = a*b
+    return c
+
+# if __name__ == '__main__':
+#    print('a+b=',plus(a,b))
+#    print('a-b=',subtract(a,b))
